3d5f10eb5d9b978fe51f31e2b144657cd531e018
MilicaKostic4 <mica.kostiic@gmail.com>
2023-08-28 15:30:47 +0200

    Ispravljene greske

--- Assets/Scripts/Core/Finish.cs
+++ Assets/Scripts/Core/Finish.cs
@@ -25,7 +25,10 @@ public class Finish : MonoBehaviour
 
     private void CompleteLevel()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1 );
+        if(SceneManager.GetActiveScene().buildIndex == 3)
+            SceneManager.LoadScene(0);
+        else
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1 );
     }
 
 
